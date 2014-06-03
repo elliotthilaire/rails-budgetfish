288464c5122e1cf374d6de5191a94c5e0a26721e
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-03 19:01:10 +1000

    create calledback to make sample accounts and categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :create_sample_accounts_categories
+
   has_many :categories
   has_many :accounts
   has_many :expenses
+
+  private
+
+  # create sample categories and accounts
+  def create_sample_accounts_categories
+  	self.accounts.create(name: 'Cash')
+  	self.categories.create(name: 'Pocket Money')
+  end
   
 end
