05a4cd4ac3eb94480159373dc63958487ef2e3dc
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-03 13:03:05 +1000

    updated category controller so users cannot acces other users categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  
   # GET /categories
   # GET /categories.json
   def index
@@ -66,7 +67,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = current_user.categories.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
