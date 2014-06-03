bacbae9d93a27674b17e6b413d5c220e9137626a
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-03 19:13:10 +1000

    made default routes better

--- app/controllers/thing_controller.rb
+++
@@ -1,9 +0,0 @@
-class ThingController < ApplicationController
-
-  before_action :authenticate_user!
-
-  def index
-  	render :index
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'thing#index'
+  get 'welcome' => 'welcome#index'
+
+  root 'expenses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
