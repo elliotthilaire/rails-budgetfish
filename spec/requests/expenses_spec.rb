4a39635c2bdafcf02e8086b12d3449fb9b33d8a8
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-05 15:55:39 +1000

    renamed specs to enabled them, and also made request specs work with devise

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
-before do
-  # Sign in as a user.
-  sign_in_as_a_valid_user
-end
+
 
 describe "Expenses" do
+
+  before do
+    # Sign in as a user.
+    sign_in_as_a_valid_user
+  end
+  
   describe "GET /expenses" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
