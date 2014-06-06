e4aca6b5a105bf658b9959b7f1aed3dac0557cb4
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-06 10:12:22 +1000

    made spec requests accounts_spec work

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Accounts", :type => :request do
+
+  before do
+  	sign_in_as_a_valid_user
+  end
+  
   describe "GET /accounts" do
     it "works! (now write some real specs)" do
       get accounts_path
