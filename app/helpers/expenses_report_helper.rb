d7e76c98c290da25ae90979e7abbdbf8b04cc427
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-06 16:20:11 +1000

    expenses report filters work

---
+++ app/helpers/expenses_report_helper.rb
@@ -0,0 +1,21 @@
+module ExpensesReportHelper
+  
+  def this_year_path
+  	start_date = Date.today.beginning_of_year.iso8601
+  	end_date = Date.today.end_of_year.iso8601
+    "#{expenses_report_url}?start_date=#{start_date}&end_date=#{end_date}" 
+  end
+
+  def this_month_path
+  	start_date = Date.today.beginning_of_month.iso8601
+  	end_date = Date.today.end_of_month.iso8601
+  	"#{expenses_report_url}?start_date=#{start_date}&end_date=#{end_date}"
+  end
+
+  def last_month_path
+  	start_date = Date.today.months_ago(1).beginning_of_month.iso8601
+  	end_date = Date.today.months_ago(1).end_of_month.iso8601
+  	"#{expenses_report_url}?start_date=#{start_date}&end_date=#{end_date}"
+  end
+
+end
