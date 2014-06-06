c0b1e269764db8881da83644a52c118ecea92b0c
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-06 15:43:01 +1000

    expenses_report working but hackey

--- app/controllers/expenses_report_controller.rb
+++ app/controllers/expenses_report_controller.rb
@@ -1,10 +1,24 @@
 class ExpensesReportController < ApplicationController
 
   def index
-    @categories = current_user.categories
+
+    expenses = current_user.expenses
+
+    # apply filters
+    @start_date = params[:start_date].to_date rescue nil
+    @end_date = params[:end_date].to_date rescue nil
+    
+    if @start_date
+      expenses = expenses.where("date >= ?", @start_date)
+    end
+    
+    if @end_date
+      expenses = expenses.where("date <= ?", @end_date)
+    end  
+
+    @data = expenses.group(:category).sum(:amount)
+
   	render :index
   end
 
-
 end
-
