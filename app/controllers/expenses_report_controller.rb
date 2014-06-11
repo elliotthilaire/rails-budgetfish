432c1deb9af205cf67464cd3122475ddef8785d9
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-11 18:41:59 +1000

    refactored report controller to use new scopes

--- app/controllers/expenses_report_controller.rb
+++ app/controllers/expenses_report_controller.rb
@@ -4,21 +4,15 @@ class ExpensesReportController < ApplicationController
 
   def index
 
-    expenses = current_user.expenses
-
     # apply filters
     @start_date = params[:start_date].to_date rescue nil
     @end_date = params[:end_date].to_date rescue nil
     
-    if @start_date
-      expenses = expenses.where("date >= ?", @start_date)
-    end
-    
-    if @end_date
-      expenses = expenses.where("date <= ?", @end_date)
-    end  
-
-    @data = expenses.group(:category).sum(:amount)
+    @data = current_user.expenses
+                        .date_after(@start_date)
+                        .date_before(@end_date)
+                        .group(:category)
+                        .sum(:amount)
 
   	render :index
   end
