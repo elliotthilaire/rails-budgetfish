a808e49198c5da00f1c26f0d2dd92bc027c4d8da
elliotthilaire <elliott.hilaire@gmail.com>
2014-06-12 12:04:38 +1000

    fixed failing tet

--- spec/requests/incomes_spec.rb
+++ spec/requests/incomes_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Incomes", :type => :request do
+
+  before do
+    sign_in_as_a_valid_user
+  end
+
   describe "GET /incomes" do
     it "works! (now write some real specs)" do
       get incomes_path
